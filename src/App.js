e20ae6979c5d7ca943ddc051e0cc6e470dc5d7d5
Marshall Lorhan <mlorhan@gmail.com>
2024-04-11 21:46:49 -0700

    started Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import NavBar from './Components/NavBar'
 
 function App() {
   return (
+    <>
+    <NavBar />
     <div className="App">
       <h1>Hello World</h1>
     </div>
+    </>
   );
 }
 
